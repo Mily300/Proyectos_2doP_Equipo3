3e131571f0c371ea212314c363fb7af255ed3e8f
nenriquezximena-tech <nenriquez.ximena@gmail.com>
2026-04-15 16:15:24 -0600

    Add files via upload

---
+++ 21_ordenamiento_indirecto_PE/quick.cpp
@@ -0,0 +1,30 @@
+#include "quick.h"
+
+int partition(Persona arr[], int idx[], int low, int high) {
+    int pivot = arr[idx[high]].edad;
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[idx[j]].edad < pivot) {
+            i++;
+            int temp = idx[i];
+            idx[i] = idx[j];
+            idx[j] = temp;
+        }
+    }
+
+    int temp = idx[i + 1];
+    idx[i + 1] = idx[high];
+    idx[high] = temp;
+
+    return i + 1;
+}
+
+void quickSort(Persona arr[], int idx[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, idx, low, high);
+
+        quickSort(arr, idx, low, pi - 1);
+        quickSort(arr, idx, pi + 1, high);
+    }
+}
---
+++ 21_ordenamiento_indirecto_PE/quick.h
@@ -0,0 +1,9 @@
+#ifndef QUICK_H
+#define QUICK_H
+
+#include "persona.h"
+
+void quickSort(Persona arr[], int idx[], int low, int high);
+int partition(Persona arr[], int idx[], int low, int high);
+
+#endif
