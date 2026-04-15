45fe756705d3ee0f6dfeb88bd36534cdce095cb9
nenriquezximena-tech <nenriquez.ximena@gmail.com>
2026-04-15 00:28:45 -0600

    Add files via upload

---
+++ 20_ordenamiento_indirecto_POO/merge.cpp
@@ -0,0 +1,30 @@
+#include "merge.h"
+
+void merge(Persona arr[], int idx[], int left, int mid, int right) {
+    int i = left, j = mid + 1, k = 0;
+    int temp[100];
+
+    while (i <= mid && j <= right) {
+        if (arr[idx[i]].getEdad() <= arr[idx[j]].getEdad()) {
+            temp[k++] = idx[i++];
+        } else {
+            temp[k++] = idx[j++];
+        }
+    }
+
+    while (i <= mid) temp[k++] = idx[i++];
+    while (j <= right) temp[k++] = idx[j++];
+
+    for (int x = 0; x < k; x++) {
+        idx[left + x] = temp[x];
+    }
+}
+
+void mergeSort(Persona arr[], int idx[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(arr, idx, left, mid);
+        mergeSort(arr, idx, mid + 1, right);
+        merge(arr, idx, left, mid, right);
+    }
+}
---
+++ 20_ordenamiento_indirecto_POO/merge.h
@@ -0,0 +1,9 @@
+#ifndef MERGE_H
+#define MERGE_H
+
+#include "Persona.h"
+
+void mergeSort(Persona arr[], int idx[], int left, int right);
+void merge(Persona arr[], int idx[], int left, int mid, int right);
+
+#endif
