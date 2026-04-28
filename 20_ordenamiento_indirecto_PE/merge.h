279c23ec2028f0b9e1e6680c40455ae7420c9a95
nenriquezximena-tech <nenriquez.ximena@gmail.com>
2026-04-28 07:25:41 -0600

    Delete 20_ordenamiento_indirecto_PE directory

--- 20_ordenamiento_indirecto_PE/merge.h
+++
@@ -1,9 +0,0 @@
-#ifndef MERGE_H
-#define MERGE_H
-
-#include "persona.h"
-
-void mergeSort(Persona arr[], int idx[], int left, int right);
-void merge(Persona arr[], int idx[], int left, int mid, int right);
-
-#endif
--- 20_ordenamiento_indirecto_PE/persona.h
+++
@@ -1,9 +0,0 @@
-#ifndef PERSONA_H
-#define PERSONA_H
-
-struct Persona {
-    char nombre[20];
-    int edad;
-};
-
-#endif
