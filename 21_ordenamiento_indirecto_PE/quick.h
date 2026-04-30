a493a18474f7b8c51f924e342751694e0089dde8
nenriquezximena-tech <nenriquez.ximena@gmail.com>
2026-04-30 10:11:04 -0600

    Delete 21_ordenamiento_indirecto_PE directory

--- 21_ordenamiento_indirecto_PE/quick.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUICK_H
-#define QUICK_H
-
-#include "persona.h"
-
-void quickSort(Persona arr[], int idx[], int low, int high);
-int partition(Persona arr[], int idx[], int low, int high);
-
-#endif
