7fa52cc45aec672ce6ddfc7f7045d0851e354bec
nenriquezximena-tech <nenriquez.ximena@gmail.com>
2026-04-30 10:10:52 -0600

    Delete 20_ordenamiento_indirecto_POO directory

--- 20_ordenamiento_indirecto_POO/merge.h
+++
@@ -1,9 +0,0 @@
-#ifndef MERGE_H
-#define MERGE_H
-
-#include "Persona.h"
-
-void mergeSort(Persona arr[], int idx[], int left, int right);
-void merge(Persona arr[], int idx[], int left, int mid, int right);
-
-#endif
