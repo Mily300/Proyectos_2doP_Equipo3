6580e2e5b7b2e524c0b3496a7cfe900146d7657a
nenriquezximena-tech <nenriquez.ximena@gmail.com>
2026-04-30 10:11:16 -0600

    Delete 21_ordenamiento_indirecto_POO directory

--- 21_ordenamiento_indirecto_POO/quick.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUICK_H
-#define QUICK_H
-
-#include "Persona.h"
-
-void quickSort(Persona arr[], int idx[], int low, int high);
-int partition(Persona arr[], int idx[], int low, int high);
-
-#endif
