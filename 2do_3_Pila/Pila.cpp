bdf175fc11f1aca8fe2d0e9e2b7ecc02ba48e319
Mily300 <milabeth300@gmail.com>
2026-05-01 16:31:29 -0600

    Pila

---
+++ 2do_3_Pila/Pila.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "pila.h"
+using namespace std;
+
+Pila::Pila() {
+    tope = -1;
+}
+
+void Pila::push(int valor) {
+    if (tope < 4) {
+        tope++;
+        datos[tope] = valor;
+    } else {
+        cout << "Pila llena\n";
+    }
+}
+
+void Pila::pop() {
+    if (tope >= 0) {
+        tope--;
+    } else {
+        cout << "Pila vacia\n";
+    }
+}
+
+void Pila::mostrar() {
+    if (tope == -1) {
+        cout << "Pila vacia\n";
+        return;
+    }
+
+    for (int i = tope; i >= 0; i--) {
+        cout << datos[i] << endl;
+    }
+}
