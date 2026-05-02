d4df9ff6146238c89b422cbbe293ab2fb2f30864
nenriquezximena-tech <nenriquez.ximena@gmail.com>
2026-05-01 19:30:59 -0600

    Add files via upload

---
+++ 2do_18_Ordenamiento (quick sort)_POO/main.cpp
@@ -0,0 +1,75 @@
+#include "QuickSort.h"
+
+int main(){
+    QuickSort qs;
+    int opcion, n;
+
+    do{
+        cout<<"\n_______MENU_______\n";
+        cout<<"1. Enteros\n";
+        cout<<"2. Caracteres\n";
+        cout<<"3. Personas\n";
+        cout<<"4. Salir\n";
+        cout<<"Opcion: ";
+        cin>>opcion;
+
+        switch(opcion){
+
+        case 1:{
+            cout<<"Cantidad: ";
+            cin>>n;
+
+            int *arr = new int[n];
+
+            for(int i=0;i<n;i++){
+                cin>>arr[i];
+            }
+
+            qs.ordenarInt(arr,0,n-1);
+            qs.mostrarInt(arr,n);
+
+            delete[] arr;
+            break;
+        }
+
+        case 2:{
+            cout<<"Cantidad: ";
+            cin>>n;
+
+            char *arr = new char[n];
+
+            for(int i=0;i<n;i++){
+                cin>>arr[i];
+            }
+
+            qs.ordenarChar(arr,0,n-1);
+            qs.mostrarChar(arr,n);
+
+            delete[] arr;
+            break;
+        }
+
+        case 3:{
+            cout<<"Cantidad: ";
+            cin>>n;
+
+            Persona *arr = new Persona[n];
+
+            for(int i=0;i<n;i++){
+                cin>>arr[i].edad;
+                cin>>arr[i].inicial;
+            }
+
+            qs.ordenarPersona(arr,0,n-1);
+            qs.mostrarPersona(arr,n);
+
+            delete[] arr;
+            break;
+        }
+
+        }
+
+    }while(opcion!=4);
+
+    return 0;
+}
